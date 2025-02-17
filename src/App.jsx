869c8d2b6fa49c73b921d69269491241417e3c47
Tolulope Akinwande <tolulopeakinwande@Tolulopes-MacBook-Pro.local>
2025-02-17 12:46:30 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "index.css"
+import "tailwindcss"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        {<Route path='/' element={<Signup/>}/>}
+        {<Route path='/' element={<SignUp/>}/>}
       </Routes>
     </>
   )
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,2 +1,9 @@
 import React from "react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
+
+const SignUp = () => {
+    <div className="w-64 h-34 bg-green-500 text-black">
+        <p>This a green box</p>
+    </div>
+}
+export default SignUp;
