d5b3d0a5d7cf9572177c6e39532d13db762b24b5
Tolulope Akinwande <tolulopeakinwande@Tolulopes-MacBook-Pro.local>
2025-02-24 13:09:19 +0100

    signup-update

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,8 +1,14 @@
 export default function SignUp() {
     return (
         <>
-            <div className="w-64 h-34 bg-green-600 text-black">
-                <p>This a green box</p>
+            <div className="w-full h-100 bg-green-600  min-h-screen text-black flex justify-center items-center">
+                <div className="w-[430px] h-[380px] bg-white text-black rounded-lg">
+                    <div className="w-[100%] h-[50px] border">
+                        <h1>Sign Up</h1>
+                        <p className="text-sm">Enter your details below to create your account and get started.</p>            
+                    </div>
+                    <div className="w-[100%] h-[300px] bg-blue-500"></div>
+                </div>
             </div>
         </>
     );
