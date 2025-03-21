d9d76a3a350f4fc09be3cba729f4619a2ffdcd22
Tolulope Akinwande <tolulopeakinwande@Tolulopes-MacBook-Pro.local>
2025-03-22 00:37:36 +0100

    linked the sign-up and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router'
-import SignUp from './components/signup';
-import Login from './components/login';
+import LoginPage from './components/login';
+import SignUpPage from './components/signup';
+import LandingPage from './components/landingPage';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<SignUp/>}/>
-      <Route path='login' element={<Login/>}/>
+      <Route path='/' element={<SignUpPage/>}/>
+      <Route path='login' element={<LoginPage/>}/>
+      <Route path='landing' element={<LandingPage/>}/>
 
     </Routes>
   );
